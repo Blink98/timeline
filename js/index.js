95a9ce77039d67afe9e5fe5319bcfe5be52312fd
Blink98 <gs130899@gmail.com>
2020-08-09 20:14:58 +0530

    Timeline layout complete

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// const addSecBtn = document.getElementById("btn");
+// const tube = document.getElementById("tube");
+// const tubeLine = document.getElementById("line");
+
+// addSecBtn.addEventListener("click", addSection);
+
+// function addSection() {
+//     var newSec = document.createElement("div");
+//     var newLeftSection = document.createElement("div");
+//     var newRightSection = document.createElement("div");
+    
+//     newSec.classList.add("item-container");
+//     newLeftSection.classList.add("left-item");
+//     newRightSection.classList.add("right-item");
+//     newSec.appendChild(newLeftSection);
+//     newSec.appendChild(newRightSection);
+
+//     var currentTubeHeight = tube.clientHeight;
+//     var newHeight = currentTubeHeight + 480;
+//     tube.style.height = `${newHeight}px`;
+
+//     var currentTubeLineHeight = tubeLine.clientHeight;
+//     var newLineHeight = currentTubeLineHeight + 480;
+//     tubeLine.style.height = `${newLineHeight}px`;
+
+//     document.getElementById("items").appendChild(newSec);
+// }   
